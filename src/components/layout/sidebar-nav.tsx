e2243ee3f2c7db2bf02e004dea1dc4198d9357ec
czmac <wp412542@alibaba-inc.com>
2025-03-29 11:18:58 +0800

    Enhance visual distinction between left nav and conversation area in sidebar-nav.tsx

--- src/components/layout/sidebar-nav.tsx
+++ src/components/layout/sidebar-nav.tsx
@@ -19,8 +19,7 @@ export function SidebarNav({
 }: SidebarNavProps) {
   return (
     <div className={cn(
-      "flex flex-col gap-2 p-4",
-      isCollapsed && "items-center p-2",
+      "flex flex-col h-full w-[200px] bg-[#F3F4F6] border-r border-r-gray-200",
       className
     )}>
       <Button 
