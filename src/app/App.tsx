3d1b7e12096580ced20311cd52e76195d68597bb
Peiiii <wangpeiiii618@gmail.com>
2025-06-25 23:14:53 +0800

    refactor: split useConnectNavigationStore

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,37 +1,19 @@
+import { LoadingScreen } from "@/components/loading-screen";
+import { useConnectNavigationStore } from "@/core/hooks/use-connect-navigation-store";
 import { cardExtension } from "@/features/card/extensions";
 import { chatExtension } from "@/features/chat/extensions";
 import { getSettingsExtensions } from "@/features/settings/extensions";
-import { kernel } from '../core/kernel';
-import { navigationStore } from "@/core/stores/navigation-store";
 import { Suspense, useEffect } from "react";
-import { useLocation, useNavigate, useRoutes } from "react-router-dom";
+import { useRoutes } from "react-router-dom";
+import { kernel } from '../core/kernel';
 import routes from "./routes";
-import { LoadingScreen } from "@/components/loading-screen";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 (window as any).kernel = kernel;
 
 // 应用主组件
 export default function App() {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const targetPath = navigationStore((state) => state.targetPath);
-  const currentPath = navigationStore((state) => state.currentPath);
-
-  // 监听导航状态变化
-  useEffect(() => {
-    if (targetPath) {
-      navigate(targetPath);
-      navigationStore.getState().navigate(null);
-    }
-  }, [targetPath, navigate]);
-
-  // 监听路由路径变化
-  useEffect(() => {
-    if (location.pathname !== currentPath) {
-      navigationStore.getState().setCurrentPath(location.pathname);
-    }
-  }, [location.pathname, currentPath]);
+  useConnectNavigationStore();
 
   // 注册所有插件
   useEffect(() => {
